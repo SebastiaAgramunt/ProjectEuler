9ecb5875331c6525f8a3f7210472a1d77c18b407
Sebastià Agramunt <sebastia@shotl.com>
2016-09-18 19:38:51 +0200

    Changed by a function and simplified code

--- 4_Largest_palindrome_product.py
+++ 4_Largest_palindrome_product.py
@@ -5,7 +5,7 @@ Find the largest palindrome made from the product of two 3-digit numbers."""
 
 import numpy as np
 
-def palindrome(number):
+def ispalindrome(number):
 	'''Function that takes a number and returns true if it is palindrome'''
 	number=str(number)
 	if number==number[::-1]:
@@ -15,16 +15,22 @@ def palindrome(number):
 
 #compute all products starting by 999 x 999 and then get the max.
 
-palindromes=[]
-nums_involved=[]
-for i in range(999,99,-1):
-	for j in range(999,99,-1):
-		num=i*j
-		if palindrome(num):
-			print (i,j,num)
-			palindromes.append(num)
-			nums_involved.append([i,j])
-pal=np.array(palindromes)
-print pal.max()
+def largest_palindrome(longitude):
+	palindromes=[]
+	nums_involved=[]
+	n2=int(''.join([str(9) for x in range(0,longitude)]))
+	n1=int(''.join([str(9) for x in range(0,longitude-1)]))
+	for i in range(n2,n1,-1):
+		for j in range(n2,n1,-1):
+			num=i*j
+			if ispalindrome(num):
+				palindromes.append(num)
+				nums_involved.append([i,j])
+	pal=np.array(palindromes,dtype=np.int64)
+	return pal.max()
 #906609
 #[993, 913]
+
+
+n=3 #three digit
+print "Largest palindrome number made from product of %d digit numbers is %d" %(n,largest_palindrome(n))
